73c5e414c6c2e85c529b117669b884d9f3c9be14
Nguyen Trung Hieu <vuondenthanhcong11@gmail.com>
2024-12-06 16:21:26 +0700

    update message cho users vi du

--- hasaki_authenticator.py
+++ hasaki_authenticator.py
@@ -13,9 +13,6 @@ def load_list_users():
     old_users = pd.read_csv('data/old_users_login.csv')
     new_users = pd.read_csv('data/new_users_login.csv')
 
-    old_users_examples = []
-    new_users_examples = []
-
     old_users = old_users[:20]
 
     users_dict = {
@@ -33,8 +30,6 @@ def load_list_users():
             "roles": ["viewer"]
         }
 
-        old_users_examples.append(f"- username: {row['username']} - password: {row['password']}")
-
     for _, row in new_users.iterrows():
         users_dict["usernames"][row["username"]] = {
             "email": row["email"],
@@ -46,12 +41,23 @@ def load_list_users():
             "roles": ["viewer"]
         }
 
+    return users_dict
+
+@st.cache_data
+def load_examples_users():
+    old_users = pd.read_csv('data/old_users_login.csv')
+    new_users = pd.read_csv('data/new_users_login.csv')
+
+    old_users_examples = []
+    new_users_examples = []
+
+    for _, row in old_users.iterrows():
+        old_users_examples.append(f"- username: {row['username']} - password: {row['password']}")
+
+    for _, row in new_users.iterrows():
         new_users_examples.append(f"- username: {row['username']} - password: {row['password']}")
 
-    st.session_state["old_users_examples"] = old_users_examples
-    st.session_state["new_users_examples"] = new_users_examples
-
-    return users_dict
+    return old_users_examples, new_users_examples
 
 def get_authenticator():
     with open('config.yaml') as file:
@@ -91,13 +97,12 @@ def login():
     elif st.session_state['authentication_status'] is None:
         st.warning('Please enter your username and password')
 
-        if "old_users_examples" not in st.session_state:
-            load_list_users()
+        old_users_examples, new_users_examples = load_examples_users()
         
         st.write("### Ví dụ tài khoản cũ:")
-        for example in st.session_state.old_users_examples[:5]:
+        for example in old_users_examples[:5]:
             st.write(example)
         
         st.write("### Ví dụ tài khoản mới:")
-        for example in st.session_state.new_users_examples:
+        for example in new_users_examples[:5]:
             st.write(example)
